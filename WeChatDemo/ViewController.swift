7bdfdc38cacc997da8006cb8e1127ddedd857742
derek <djing@theknotww.com>
2020-07-24 10:40:28 +0800

    Initial Commit

---
+++ WeChatDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeChatDemo
+//
+//  Created by djing on 2020/7/24.
+//  Copyright © 2020 djing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
